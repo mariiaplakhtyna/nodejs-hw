9bc9d4a527149da226776479228040fd36dfdf1e
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-07-20 17:17:53 -0400

    Use MONGO_URL for MongoDB connection

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,14 +2,7 @@ import mongoose from 'mongoose';
 
 export const initMongoConnection = async () => {
   try {
-    const user = process.env.MONGODB_USER;
-    const password = process.env.MONGODB_PASSWORD;
-    const url = process.env.MONGODB_URL;
-    const db = process.env.MONGODB_DB;
-
-    await mongoose.connect(
-      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`,
-    );
+    await mongoose.connect(process.env.MONGO_URL);
 
     console.log('Mongo connection successfully established!');
   } catch (error) {
